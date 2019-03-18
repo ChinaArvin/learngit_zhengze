9d19cd1b5647c16176140d9da0888ffad4734979
Arvin <1360676187@qq.com>
2019-03-18 14:39:45 +0800

    Initial commit

---
+++ 正则表达式.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import re
+
+# string =  "xxxxxxxxxxxxxxxxaaaxxxxxx entry '某某内容' for ddddddd"
+#
+#
+# result =  re.findall(".*aaa(.*).*",string)
+#
+# for x in result:
+#
+#     print x
+
+a="ABCDEFGHIJABCDEFGHIJABCDEF/GHIJ"
+b="/"
+print(u"在a中查找最后一个b后面的字符:"+a[a.rfind(b)+1:])
+
